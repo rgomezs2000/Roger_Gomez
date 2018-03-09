a4e42d5afd285d87ce05ae527351ec177ea1f918
rgomezs2000 <rogergomezs2003@gmail.com>
2018-03-09 02:25:47 -0400

    csmbios en la evaluacion

---
+++ ConexionBaseDatos/App_Code/ConexionBaseDatosasmx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data;
+using System.Data.Odbc;
+
+/// <summary>
+/// Descripción breve de ConexionBaseDatosasmx
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.ComponentModel.ToolboxItem(false)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+//[System.Web.Script.Services.ScriptService]
+public class ConexionBaseDatosasmx : System.Web.Services.WebService
+{
+
+    [WebMethod]
+    public Corresponsales ListarCorresponsales()
+    {
+        Corresponsales corresponsales = new Corresponsales();
+        using (SqlConnection conn = new SqlConnection(connectionString: "Data Source=./SQLEXPRESS;Initial Catalog=cmmoneycash;Integrated Security=SSPI;"))
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+                cmd.CommandText = "EXEC ListarCorresponsales";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        corresponsales.CorCorresponsalId = int.Parse(reader["COR_CORRESPONSAL_ID"].ToString());
+                        corresponsales.CorNombre = reader["CONT_OFICINA"].ToString();
+                    }
+                }
+
+                conn.Close();
+                
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        return corresponsales;
+    }
+
+}
---
+++ ConexionBaseDatos/App_Code/Corresponsales.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Corresponsales
+/// </summary>
+public class Corresponsales
+{
+    public Corresponsales()
+    {
+        this.corCorresponsalId = 0;
+        this.corNombre = "";
+    }
+
+    public Corresponsales(int corCorresponsalId, string corNombre)
+    {
+        this.corCorresponsalId = CorCorresponsalId;
+        this.corNombre = CorNombre;
+    }
+
+    private int corCorresponsalId;
+
+    public int CorCorresponsalId
+    {
+        get
+        {
+            return corCorresponsalId;
+        }
+
+        set
+        {
+            corCorresponsalId = value;
+        }
+    }
+
+    public string CorNombre
+    {
+        get
+        {
+            return corNombre;
+        }
+
+        set
+        {
+            corNombre = value;
+        }
+    }
+
+    private string corNombre;
+}
